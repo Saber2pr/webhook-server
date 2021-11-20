38f13a9cba591b4671e7ec7bf376e928eb8f25d5
saber2pr <saber2pr@gmail.com>
2021-11-20 18:20:28 +0800

    chore: update

--- src/cli.ts
+++
@@ -1,7 +0,0 @@
-import runInWorkspace from '@saber2pr/cli'
-import { join } from 'path'
-
-runInWorkspace({
-  name: 'cli',
-  libRoot: join(__dirname, '..'),
-})
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { parseUrlParam } from './utils/parseUrlParam'
 import { exec } from 'child_process'
 import { createServer } from 'http'
 import { promisify } from 'util'
+import { join } from 'path'
 
 const Exec = promisify(exec)
 
@@ -10,7 +11,11 @@ const server = createServer(async (req, res) => {
     const params = parseUrlParam<{ command: string; args: string }>(req.url)
     const command = params?.command || ''
     const args = params?.args || ''
-    await Exec(`yarn cli ${command} ${args.split(',').join(' ')}`)
+    await Exec(
+      `sh ${join(__dirname, `../scripts/${command}.sh`)} ${args
+        .split(',')
+        .join(' ')}`
+    )
 
     res.end(JSON.stringify({ success: true }))
   } catch (error) {
