0755c0b8d2df54999b404891ea8448c3354575b9
saber2pr <saber2pr@gmail.com>
2021-11-20 18:12:56 +0800

    chore: update

--- scripts/deploy-blog.sh
+++ scripts/deploy-blog.sh
@@ -6,7 +6,4 @@ old_images="$(docker images | grep $image_name | grep -oE "[0-9a-z]{12}" | tr '\
 
 # pull image
 tag=$1
-docker pull saber2pr/next-ssr-blog:$tag > ./deploy-blog.log
-
-# remove old containers, run new container
-docker rm -f $old && docker run --restart=always -d -p 88:3000 -it saber2pr/next-ssr-blog:$tag > ./deploy-blog.log && docker rmi $old_images
+docker pull saber2pr/next-ssr-blog:$tag > ./deploy-blog.log && docker rm -f $old && docker run --restart=always -d -p 88:3000 -it saber2pr/next-ssr-blog:$tag > ./deploy-blog.log && docker rmi $old_images
