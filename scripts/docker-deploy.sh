851df5907f8068cace180aed3ee207611274e1df
saber2pr <saber2pr@gmail.com>
2021-11-21 12:32:01 +0800

    refact: deploy param

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,12 +1,11 @@
-image_name="next-ssr-blog"
-
-# pull image
-tag=$1
+image_name=$1
+tag=$2
+port=$3
 
 # free memory
 docker rm -f $(docker ps -a | grep $image_name | grep -oE "[0-9a-z]{12}" | tr '\n' ' ') \
 && docker rmi $(docker images | grep $image_name | grep -oE "[0-9a-z]{12}" | tr '\n' ' ')
 
 # pull and run
-docker pull saber2pr/next-ssr-blog:$tag > ./deploy-blog.log \
-&& docker run --restart=always -d -p 88:3000 -it saber2pr/next-ssr-blog:$tag > ./deploy-blog.log
+docker pull $image_name:$tag > ./deploy-blog.log \
+&& docker run --restart=always -d -p $port -it $image_name:$tag > ./deploy-blog.log
--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,30 @@ import { join } from 'path'
 
 const Exec = promisify(exec)
 
+const parseDeployParams = (deploy: string = '') => {
+  const [image, port] = deploy.split(',')
+  const [image_name, tag] = image.split(':')
+  return {
+    image_name,
+    tag,
+    port,
+  }
+}
+
 const server = createServer(async (req, res) => {
   try {
-    const params = parseUrlParam<{ command: string }>(req.url)
-    const command = params?.command || ''
-    const [script, args] = command.split(':')
+    const params = parseUrlParam<{ deploy: string }>(req.url)
+    //  image_name:tag,out-port:inner-port
+    const deploy = params?.deploy || ''
 
+    const { image_name, tag, port } = parseDeployParams(deploy)
+
+    // run shell
     await Exec(
-      `sh ${join(__dirname, `../scripts/${script}.sh`)} ${args
-        .split(',')
-        .join(' ')}`
+      `sh ${join(
+        __dirname,
+        `../scripts/docker-deploy.sh`
+      )} ${image_name} ${tag} ${port}`
     )
 
     res.end(JSON.stringify({ success: true }))
